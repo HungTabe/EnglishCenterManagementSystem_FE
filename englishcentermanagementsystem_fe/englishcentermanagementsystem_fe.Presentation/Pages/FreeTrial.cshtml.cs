98f7e8c2013b6c25bd79fce9c6208984e9ea057a
tuantu04 <tuantu04@gmail.com>
2025-06-29 17:15:46 +0700

    [T3] Setup MarketingPage

---
+++ englishcentermanagementsystem_fe/englishcentermanagementsystem_fe.Presentation/Pages/FreeTrial.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace englishcentermanagementsystem_fe.Presentation.Pages
+{
+    public class FreeTrialModel : PageModel
+    {
+        [BindProperty]
+        public string Email { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // TODO: Xử lý email (lưu vào database, gửi email, v.v.)
+            return Page();
+        }
+    }
+}
--- englishcentermanagementsystem_fe/englishcentermanagementsystem_fe.Presentation/wwwroot/js/site.js
+++ englishcentermanagementsystem_fe/englishcentermanagementsystem_fe.Presentation/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            const offsetTop = target.offsetTop - 70; // Account for fixed navbar
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
 
-// Write your JavaScript code.
+// Update navbar on scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('shadow');
+    } else {
+        navbar.classList.remove('shadow');
+    }
+});
+
+// Form submission handler
+document.querySelector('#cta form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const email = this.querySelector('input[type="email"]').value;
+    if (email) {
+        alert('Thank you for your interest! We\'ll be in touch soon.');
+        this.reset();
+    }
+});
