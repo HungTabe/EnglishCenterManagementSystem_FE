aca99e6c7e60f207de67fc9c67f62e40b46b5162
hungtabe <trandinhhung717@gmail.com>
2025-07-23 12:47:06 +0700

    [M2.T20] Develop Salary Calculation Page

---
+++ englishcentermanagementsystem_fe/englishcentermanagementsystem_fe.Presentation/Pages/SalaryCalculation.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class SalaryCalculationModel : PageModel
+{
+    public void OnGet()
+    {
+        // No server-side logic needed for AJAX approach
+    }
+} 
